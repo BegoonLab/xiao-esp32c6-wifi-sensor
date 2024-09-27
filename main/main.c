e62a91e9e0f398b5fb49c9e3d6476d512d28fd8e
Alexander Begoon <alex@begoonlab.tech>
2024-09-27 12:29:33 +0200

    Init commit

---
+++ main/main.c
@@ -0,0 +1,14 @@
+/**
+ * @file    main.c
+ * @author  Alexander Begoon (<a href="mailto:alex\@begoonlab.tech">alex\@begoonlab.tech</a>)
+ * @date    27 September 2024
+ * 
+ * @copyright Copyright (c) 2024 <a href="https://begoonlab.tech">BegoonLab</a>.
+ *            All rights reserved.
+ */
+
+#include "sdkconfig.h"
+
+void app_main(void) {
+
+}

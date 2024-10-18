8dd6585bbf36a06711be2abd63ec8bf7c0bee0fb
Alexander Begoon <alex@begoonlab.tech>
2024-10-18 17:45:04 +0200

    Refactor WiFi part Add partitions scheme Add default config Add ZigBee menu

--- main/sensor_mqtt.h
+++ main/sensor_mqtt.h
@@ -14,9 +14,9 @@
 
 #include "esp_event.h"
 #include "esp_log.h"
-
+#include <cJSON.h>
 #include "mqtt_client.h"
-
+#include "sys/time.h"
 #include "freertos/event_groups.h"
 
 #ifdef CONFIG_MQTT_QOS_0
@@ -28,6 +28,7 @@
 #endif
 
 #define MQTT_TOPIC_MAX_LEN 256
+#define MQTT_MSG_MAX_LEN 512
 
 void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
 
@@ -35,3 +36,5 @@ esp_mqtt_client_handle_t init_mqtt_client();
 
 void mqtt_publish(esp_mqtt_client_handle_t client, const char *data);
 void stop_mqtt(esp_mqtt_client_handle_t client);
+void mqtt_prepare_json(char *json_string, int rssi, double battery_voltage, double temperature, double humidity,
+                       double pressure, struct timeval start_to_connect, struct timeval end_to_connect);
---
+++ main/sensor_zb.h
@@ -0,0 +1,13 @@
+/**
+ * @file    sensor_zb.h
+ * @author  Alexander Begoon (<a href="mailto:alex\@begoonlab.tech">alex\@begoonlab.tech</a>)
+ * @date    18 October 2024
+ * @brief   //TODO
+ * 
+ * @details //TODO
+ * 
+ * @copyright Copyright (c) 2024 <a href="https://begoonlab.tech">BegoonLab</a>.
+ *            All rights reserved.
+ */
+
+#pragma once
